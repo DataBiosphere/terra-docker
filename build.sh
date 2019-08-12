439f1e8d1122b715ebfdf26e89bbe78e8cd22717
Qi Wang <qi@broadinstitute.org>
2019-08-12 16:21:05 -0400

    add build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Example: ./build.sh terra-jupyter-base
+
+IMAGE_DIR=$1
+
+TAG_NAME=$(git log --pretty=format:'%h' -n 1)
+docker image build ./$IMAGE_DIR --tag us.gcr.io/broad-dsp-gcr-public/$IMAGE_DIR:$TAG_NAME
+docker push us.gcr.io/broad-dsp-gcr-public/$IMAGE_DIR:$TAG_NAME
