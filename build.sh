251dd66c435ad1dd4b791075670b0a85ed5312e5
jdcanas <jcanas@broadinstitute.com>
2019-09-26 17:21:02 -0400

    updated script to provide versions

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # Run `gcloud auth configure-docker --quiet` before running this script
 # Example: ./build.sh terra-jupyter-base
 
-PYTHON_IMAGES=("terrra-jupyter-hail" "terra-jupyter-python" "terra-jupyter-base" "terra-jupyter-bioconductor")
-R_IMAGES=("terra-jupyter-r", "terra-jupyter-bioconductor")
+PYTHON_IMAGES="terrra-jupyter-hail terra-jupyter-python terra-jupyter-base terra-jupyter-bioconductor"
+R_IMAGES="terra-jupyter-r terra-jupyter-bioconductor"
 
 function contains() {
     local n=$#
@@ -42,25 +42,29 @@ gcloud auth activate-service-account --key-file=dspci-wb-gcr-service-account.jso
 
 docker image build ./$IMAGE_DIR --tag $REPO/$IMAGE_DIR:$TAG_NAME --tag $REPO/$IMAGE_DIR:$VERSION \
     && docker push $REPO/$IMAGE_DIR:$TAG_NAME \
-    && docker push $REPO/$IMAGE_DIR:$VERSION
+    && docker push $REPO/$IMAGE_DIR:$VERSIONq
 
-docker run --rm -itd -u root -e PIP_USER=false --entrypoint='/bin/bash' --name $IMAGE_DIR $REPO/$IMAGE_DIR:$TAG_NAME
+docker run --rm -itd -u root -e PIP_USER=false --entrypoint='/bin/bash' --name $IMAGE_DIR $REPO/$IMAGE_DIR:$VERSION
 
 PYTHON_OUTPUT_FILE="$IMAGE_DIR-$VERSION-python-packages.txt"
 R_OUTPUT_FILE="$IMAGE_DIR-$VERSION-r-packages.txt"
 
+## update remote documentation
 
-#update remote documentation
 #determine if we should produce python or R documentation for a package, and upload it to a bucket if so
-if [ $(contains "${PYTHON_IMAGES[@]}" "$IMAGE_DIR") == "true" ]; then
-    docker exec $IMAGE_DIR pip list > $PYTHON_OUTPUT_FILE;
+if [[ $PYTHON_IMAGES == *"$IMAGE_DIR"* ]]; then
+    docker exec $IMAGE_DIR python3 --version > $PYTHON_OUTPUT_FILE;
+    docker exec $IMAGE_DIR pip list >> $PYTHON_OUTPUT_FILE;
+
     echo "gsutil cping $PYTHON_OUTPUT_FILE to $DOCUMENTATION_BUCKET"
     gsutil cp $PYTHON_OUTPUT_FILE $DOCUMENTATION_BUCKET
 fi
 
-if [ $(contains "${R_IMAGES[@]}" "$IMAGE_DIR") == "true" ]; then
-    #R is a wonderful language that requires you to execute code within a shell to find the packages
-    docker exec $IMAGE_DIR sh -c "echo 'ip <- as.data.frame(installed.packages()[,c(1,3:4)]) \n rownames(ip) <- NULL \n ip <- ip[is.na(ip\$Priority),1:2,drop=FALSE] \n print(ip, row.names=FALSE)' > temp.r; Rscript temp.r; rm temp.r" > $R_OUTPUT_FILE
+if [[ $R_IMAGES == *"$IMAGE_DIR"* ]]; then
+    docker exec $IMAGE_DIR R --version | head -1 > $R_OUTPUT_FILE
+    #R requires you execute code within a shell to find the packages
+    docker exec $IMAGE_DIR sh -c "echo 'ip <- as.data.frame(installed.packages()[,c(1,3:4)]) \n rownames(ip) <- NULL \n ip <- ip[is.na(ip\$Priority),1:2,drop=FALSE] \n print(ip, row.names=FALSE)' > temp.r; Rscript temp.r; rm temp.r" >> $R_OUTPUT_FILE
+
     echo "gsutil cping $R_OUTPUT_FILE to $DOCUMENTATION_BUCKET"
     gsutil cp $R_OUTPUT_FILE $DOCUMENTATION_BUCKET
 fi
