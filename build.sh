5bf66d6ef4c581879a4a4be757c2675e199f4f97
jdcanas <jcanas@broadinstitute.com>
2019-12-17 10:34:38 -0500

    changes finalized

--- build.sh
+++ build.sh
@@ -27,11 +27,10 @@ fi
 
 # will fail if you are not gcloud authed as dspci-wb-gcr-service-account or secret/dsde/firecloud/common/image-build-account.json 
 # the below works locally but not on jenkins for some reason, using a different account until a resolution is found
-gcloud auth revoke
-docker run --rm  -v $VAULT_LOCATION:/root/.vault-token:ro broadinstitute/dsde-toolbox:latest vault read --format=json secret/dsde/dsp-techops/common/dspci-wb-gcr-service-account.json | jq .data > dspci-wb-gcr-service-account.json
-gcloud auth activate-service-account --key-file=dspci-wb-gcr-service-account.json
-# docker run --rm  -v /etc/vault-token-dsde:/root/.vault-token:ro broadinstitute/dsde-toolbox:latest vault read --format=json secret/dsde/firecloud/common/image-build-account.json | jq .data > image-build-account.json
-# gcloud auth activate-service-account --key-file=image-build-account.json
+# docker run --rm  -v $VAULT_LOCATION:/root/.vault-token:ro broadinstitute/dsde-toolbox:latest vault read --format=json secret/dsde/dsp-techops/common/dspci-wb-gcr-service-account.json | jq .data > dspci-wb-gcr-service-account.json
+# gcloud auth activate-service-account --key-file=dspci-wb-gcr-service-account.json
+docker run --rm  -v /etc/vault-token-dsde:/root/.vault-token:ro broadinstitute/dsde-toolbox:latest vault read --format=json secret/dsde/firecloud/common/image-build-account.json | jq .data > image-build-account.json
+gcloud auth activate-service-account --key-file=image-build-account.json
 gcloud auth configure-docker --quiet
 
 docker image build ./$IMAGE_DIR --tag $GCR_IMAGE_REPO/$IMAGE_DIR:$TAG_NAME --tag $GCR_IMAGE_REPO/$IMAGE_DIR:$VERSION \
