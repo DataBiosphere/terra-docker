585bda9a1b512980f58d293d7300601139fa1fea
Robert Title <rtitle@broadinstitute.org>
2021-09-10 21:23:02 -0400

    Fix pysam and add a test case

--- terra-jupyter-python/tests/smoke_test.py
+++ terra-jupyter-python/tests/smoke_test.py
@@ -48,3 +48,38 @@ def test_tensorflow_hello_world():
   hello = tf.constant('Hello, TensorFlow!')
   sess = tf.Session()
   print(sess.run(hello))
+
+def test_pysam():
+  import pysam
+
+  # Create BAM file from scratch
+  # Code stolen from https://pysam.readthedocs.io/en/latest/usage.html#creating-bam-cram-sam-files-from-scratch
+  header = { 'HD': {'VN': '1.0'},
+            'SQ': [{'LN': 1575, 'SN': 'chr1'},
+                   {'LN': 1584, 'SN': 'chr2'}] }
+
+  file_name = "out.bam"
+  with pysam.AlignmentFile(file_name, "wb", header=header) as outf:
+    a = pysam.AlignedSegment()
+    a.query_name = "read_28833_29006_6945"
+    a.query_sequence="AGCTTAGCTAGCTACCTATATCTTGGTCTTGGCCG"
+    a.flag = 99
+    a.reference_id = 0
+    a.reference_start = 32
+    a.mapping_quality = 20
+    a.cigar = ((0,10), (2,1), (0,25))
+    a.next_reference_id = 0
+    a.next_reference_start=199
+    a.template_length=167
+    a.query_qualities = pysam.qualitystring_to_array("<<<<<<<<<<<<<<<<<<<<<:<9/,&,22;;<<<")
+    a.tags = (("NM", 1),
+              ("RG", "L1"))
+    outf.write(a)
+
+  # Verify output file exists
+  assert os.path.isfile(file_name)
+
+  # Call samtools to sort the file
+  # This will fail if the file is not a valid BAM file
+  pysam.sort("-o", "sorted.bam", file_name)
+  assert os.path.isfile("sorted.bam")
