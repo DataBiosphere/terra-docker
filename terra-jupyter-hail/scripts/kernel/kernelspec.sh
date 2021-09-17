d3ce924f6f6d1dbf8e68e119ca378e2078f8b771
Robert Title <rtitle@broadinstitute.org>
2021-09-17 09:18:13 -0400

    IA-2957: Fix Bioconcutor label plus a couple other non-functional fixes (#254)
    * IA-2957: Fix Bioconcutor label plus a couple other non-functional fixes
    
    * Remove COPY

--- terra-jupyter-hail/scripts/kernel/kernelspec.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-TMP_KERNELSPEC_DIR=$1
-KERNELSPEC_HOME=$2
-
-# Replace the contents of the Python kernel scripts
-sed -e 's/${PY_VERSION}/3/g' -e 's|${JUPYTER_HOME}|'${JUPYTER_HOME}'|g' ${TMP_KERNELSPEC_DIR}/python_kernelspec.tmpl > ${KERNELSPEC_HOME}/python3/kernel.json
