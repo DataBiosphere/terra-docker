118ca5a0b0ca855515c8cc015baeef35f8141266
Peter Lavigne <peterlavigne@google.com>
2023-05-18 14:06:52 -0400

    Remove deprecated gapi usage
    How I validated this:
    
    1. I installed the image:
    
    `docker pull us.gcr.io/broad-dsp-gcr-public/terra-jupyter-base`
    
    2. I ran the command as a root user:
    
    `docker run --user root -d -p 9000:8000 -v ~/src/terra-docker:/home/jupyter -it us.gcr.io/broad-dsp-gcr-public/terra-jupyter-base --allow-root`
    
    3. I opened the Jupyter terminal and ran the following commands:
    
    `cd terra-jupyter-base/custom`
    `jupyter nbextension install google_sign_in.js --symlink`
    `jupyter nbextension enable google_sign_in`
    
    4. I opened Jupyter and observed that the extension was loaded
    
    5. By manually setting `params.googleClientId = "fake"` in the extension's `init` function,
       I was able to compare the behavior with and without this commit.
    
    5a. Without this commit, I observed (via the network tab) the auth API
        being (unsuccessfully) called.
    
    5b. With this commit, that call is no longer made.

--- terra-jupyter-base/custom/google_sign_in.js
+++ terra-jupyter-base/custom/google_sign_in.js
@@ -1,7 +1,13 @@
 /*
- * This library is designed to run as a Jupyter/JupyterLab extension to refresh the user's
- * Google credentials while using a notebook. This flow is described in more detail here:
- * https://github.com/DataBiosphere/leonardo/wiki/Connecting-to-a-Leo-Notebook#token-refresh
+ * This library is a Jupyter/JupyterLab extension that performs periodic jupyter status checks.
+ *
+ * Originally, this extension refreshed the user's Google credentials while using a notebook.
+ * This flow is described in more detail here:
+ * https://github.com/DataBiosphere/leonardo/wiki/Connecting-to-a-Leo-Notebook#token-refresh.
+ * However, the google auth library being used was slated to be sunset:
+ * https://developers.google.com/identity/sign-in/web/deprecation-and-sunset.
+ * As a result, the auth code has been removed. Leonardo's clients now refresh tokens independently.
+ * Further investigation is needed to determine if this library is still needed.
  *
  * Note since this runs inside both Jupyter and JupyterLab, it should not use any
  * libraries/functionality that exists in one but not the other. Examples: node, requireJS.
@@ -104,29 +110,6 @@ function xhttpGet(url, callback) {
 }
 
 function startTimer() {
-  loadGapi('auth2', function() {
-    function doAuth() {
-      if (params.googleClientId) {
-        gapi.auth2.authorize({
-          'client_id': params.googleClientId,
-          'scope': 'openid profile email',
-          'login_hint': params.loginHint,
-          'prompt': 'none'
-        }, function(result) {
-          if (result.error) {
-            console.error('Error occurred authorizing with Google: ' + result.error);
-            return;
-          }
-          setCookie(result.access_token, result.expires_in);
-        });
-      }
-    }
-
-    // refresh token every 3 minutes
-    console.log('Starting token refresh timer');
-    setInterval(doAuth, 180000);
-  });
-
   function statusCheck() {
     const url = `/proxy/${params.googleProject}/${params.clusterName}/jupyter/api/status`;
     // logging the statusCheck can be noisy, and it will tell us in the console if it fails
@@ -134,31 +117,6 @@ function startTimer() {
   }
 
   setInterval(statusCheck, 60000);
-}
-
-// Note: this should match
-// https://github.com/DataBiosphere/leonardo/blob/develop/http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/CookieHelper.scala
-function setCookie(token, expiresIn) {
-  document.cookie = 'LeoToken=' + token + '; Max-Age=' + expiresIn + '; Path=/; Secure; SameSite=None';
-}
-
-function loadGapi(googleLib, continuation) {
-  console.log('Loading Google APIs');
-  // Get the gapi script from Google.
-  const gapiScript = document.createElement('script');
-  gapiScript.src = 'https://apis.google.com/js/api.js';
-  gapiScript.type = 'text/javascript';
-  gapiScript.async = true;
-
-  // Load requested API scripts onto the page.
-  gapiScript.onload = function() {
-    console.log('Loading Google library \'' + googleLib + '\'');
-    gapi.load(googleLib, continuation);
-  };
-  gapiScript.onerror = function() {
-    console.error('Unable to load Google APIs');
-  };
-  document.head.appendChild(gapiScript);
 }
 
 function init() {
