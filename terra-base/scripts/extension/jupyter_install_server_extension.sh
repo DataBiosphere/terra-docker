d5dc18a8c40712c26a9dd3ff5372395ffd1b423c
LizBaldo <baldo@broadinstitute.org>
2023-10-12 14:59:43 -0400

    use nbclassic instead of notebook to maintain backward compatibility with nbextensions

--- terra-base/scripts/extension/jupyter_install_server_extension.sh
+++ terra-base/scripts/extension/jupyter_install_server_extension.sh
@@ -8,5 +8,5 @@ if [ -n "$1" ]; then
   mkdir ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
   tar -xzf ${JUPYTER_EXTENSION} -C${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
   pip3 install -e ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
-  sudo -E -u jupyter jupyter serverextension enable --py ${JUPYTER_EXTENSION_NAME}
+  sudo -E -u jupyter serverextension enable --py ${JUPYTER_EXTENSION_NAME}
 fi
--- terra-base/scripts/extension/jupyter_pip_install_combined_extension.sh
+++ terra-base/scripts/extension/jupyter_pip_install_combined_extension.sh
@@ -5,7 +5,7 @@ set -e
 if [ -n "$1" ]; then
   JUPYTER_EXTENSION=$1
   pip3 install ${JUPYTER_EXTENSION}
-  sudo -E -u jupyter jupyter serverextension enable --py ${JUPYTER_EXTENSION}
-  sudo -E -u jupyter jupyter nbextension install --py ${JUPYTER_EXTENSION} --user
-  sudo -E -u jupyter jupyter nbextension enable --py ${JUPYTER_EXTENSION}
+  sudo -E -u jupyter serverextension enable --py ${JUPYTER_EXTENSION}
+  sudo -E -u jupyter nbextension install --py ${JUPYTER_EXTENSION} --user
+  sudo -E -u jupyter nbextension enable --py ${JUPYTER_EXTENSION}
 fi
