90e91505b8d6d1f125022b4c4ab18e3404286a42
Qi Wang <qi@broadinstitute.org>
2025-03-11 11:31:22 -0400

    remove docker

--- terra-jupyter-aou/upgrade_jupyter_server.sh
+++ terra-jupyter-aou/upgrade_jupyter_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec jupyter-server pip install --upgrade jupyter-server
-docker exec -u 0 jupyter-server jupyter server extension enable --py qiime2 --sys-prefix
+pip install --upgrade jupyter-server
+jupyter server extension enable --py qiime2 --sys-prefix
 
