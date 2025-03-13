6a74358a829aaa93b538dbc9b7366100e3d8b079
Qi Wang <qi@broadinstitute.org>
2025-03-13 17:06:39 -0400

    maybe

--- terra-jupyter-aou/upgrade_jupyter_server.sh
+++ terra-jupyter-aou/upgrade_jupyter_server.sh
@@ -3,5 +3,5 @@
 set -e -x
 
 sudo -E -u jupyter pip install --upgrade jupyter-server
-jupyter server extension enable --py qiime2 --sys-prefix
+sudo -E -u jupyter jupyter server extension enable --py qiime2 --sys-prefix
 chown -R jupyter:users $JUPYTER_USER_HOME/
