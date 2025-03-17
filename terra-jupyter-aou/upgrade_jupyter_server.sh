e9445bed50861007d8781ef0462c39b261a60e64
Qi Wang <qi@broadinstitute.org>
2025-03-17 10:59:35 -0400

    try

--- terra-jupyter-aou/upgrade_jupyter_server.sh
+++ terra-jupyter-aou/upgrade_jupyter_server.sh
@@ -3,6 +3,6 @@
 set -e -x
 
 chown -R jupyter:users $JUPYTER_USER_HOME/
-sudo -E -u jupyter pip install --upgrade jupyter-server
+sudo -E -u jupyter /opt/conda/bin/pip install --upgrade jupyter-server
 chown -R jupyter:users /opt/conda/etc/jupyter/
 sudo -E -u jupyter /opt/conda/bin/jupyter server extension enable --py qiime2 --sys-prefix
